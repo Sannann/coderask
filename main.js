f10785e1940d368002d99a0a54d726aa48b7fe83
S.Cioli <109931130+Xpok@users.noreply.github.com>
2023-08-08 19:31:16 -0300

    ,

---
+++ main.js
@@ -0,0 +1,6 @@
+let Zombies_Points = prompt("How many zombies are there");
+while (Zombies_Points != "ESC") 
+    let Double_Points = prompt("Do you have double points?");
+
+    if (Double_Points == "Yes") let DoublePointsMultiplier = 2{ alert("You have " + ZombiesPoints *130* DoublePointsMultiplier);
+}else{ let DoublePointsMultiplier = 1 } alert("You have "ZombiesPoints * 130 * DoublePointsMultiplier)
